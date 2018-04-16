e5ac683d5a9a487c191bb4779769a4520e205e71
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-04-16 15:47:21 +0900

    args modified

--- create_dataset.py
+++ create_dataset.py
@@ -14,22 +14,18 @@ import Tools.func as F
 
 def command():
     parser = argparse.ArgumentParser(description=help)
-    parser.add_argument('jpeg', nargs='+',
-                        help='使用する画像のパス')
-    parser.add_argument('--channel', '-c', type=int, default=1,
-                        help='画像のチャンネル数 [default: 1 channel]')
-    parser.add_argument('--img_size', '-s', type=int, default=32,
-                        help='生成される画像サイズ [default: 32 pixel]')
-    parser.add_argument('--round', '-r', type=int, default=1000,
-                        help='切り捨てる数 [default: 1000]')
-    parser.add_argument('--quality', '-q', type=int, default=5,
-                        help='画像の圧縮率 [default: 5]')
+    parser.add_argument('ref_img',
+                        help='使用する参照画像のパス')
+    parser.add_argument('cap_img', nargs='+',
+                        help='使用するキャプチャ画像のパス')
     parser.add_argument('--train_per_all', '-t', type=float, default=0.9,
                         help='画像数に対する学習用画像の割合 [default: 0.9]')
     parser.add_argument('-o', '--out_path', default='./result/',
-                        help='・ (default: ./result/)')
+                        help='データの保存先 (default: ./result/)')
     parser.add_argument('-hg', '--homography', default='',
-                        help='・ ')
+                        help='使用したい透視変換行列のパス ')
+    parser.add_argument('--view', action='store_true',
+                        help='cv2.imshow()で変換結果を表示する')
     return parser.parse_args()
 
 
@@ -67,55 +63,49 @@ def alignImages(img1, img2,
         return img1, np.zeros((3, 3))
 
 
-def main(args):
+def main(ref, cam, out_path, num, homography, view):
     # Read reference image
-    ref_name = args.jpeg[0]
+    ref_name = ref
     print('Reading reference image : ', ref_name)
     ref = cv2.imread(ref_name, cv2.IMREAD_COLOR)
 
     # Read image to be aligned
-    img_name = args.jpeg[1]
+    img_name = cam
     print('Reading image to align : ', img_name)
     img = cv2.imread(img_name, cv2.IMREAD_COLOR)
-
-    num = 1
 
     print('Aligning images ...')
     # Registered image will be resotred in imReg.
     # The estimated homography will be stored in h.
     st = time.time()
-    for i in range(num):
+    if args.homography == '':
         dst, h = alignImages(ref, img)
+    else:
+        arr = np.load(args.homography)
+        height, width, channels = img.shape
+        h = arr['h']
+        dst = cv2.warpPerspective(ref, h, (width, height))
 
     print(time.time() - st)
 
-    if args.homography != '':
-        arr = np.load(args.homography)
-        print(arr['h'])
-        height, width, channels = img.shape
-        h = arr['h']
-        st = time.time()
-        for i in range(num):
-            dst = cv2.warpPerspective(ref, h, (width, height))
-
-        print(time.time() - st)
-
     # Write aligned image to disk.
-    outFileName = F.getFilePath(args.out_path, 'aligned', '.jpg')
+    outFileName = F.getFilePath(args.out_path, 'aligned_'+str(num).zfill(5), '.jpg')
     print('Saving aligned image : ', outFileName)
     cv2.imwrite(outFileName, dst)
 
     # Print estimated homography
     print('Estimated homography : \n',  h)
-    outFileName = F.getFilePath(args.out_path, 'h', '.npz')
+    outFileName = F.getFilePath(args.out_path, 'h_'+str(num).zfill(5), '.npz')
     np.savez(outFileName, h=h)
 
-    cv2.imshow('view', dst)
-    cv2.waitKey()
+    if args.view:
+        cv2.imshow('view', dst)
+        cv2.waitKey()
 
 
 if __name__ == '__main__':
 
     args = command()
     F.argsPrint(args)
-    main(args)
+    for i, cap in enumerate(args.cap_img):
+        main(args.ref_img, cap, args.out_path, i, args.homography, args.view)
